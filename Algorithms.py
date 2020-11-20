cbb870aa420a52a8f1a5ceb6f6f38cb8829492e3
rjmpt <67809770+rjmpt@users.noreply.github.com>
2020-11-19 19:35:08 -0500

    Update Algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -1,3 +1,5 @@
+#this file contains the hyperparameter specification for the 5 different algorithms that I ran and evaluated in my project
+#all hyperparameter specifications as detailed in section 3.2 of the project report can be found below
 import helper 
 import distributions
 import statistical_divergences
@@ -28,14 +30,14 @@ def get_algorithm(algorithm_name):
         ## DCGAN
         from architectures import get_dcgan_architectures
         from distributions import DiagonalGaussianDistribution
-        from statistical_divergences import FDivKL
+        from statistical_divergences import FDivKL #KL divergence as the f-divergence
 
         algorithm_args = helper.AttrDict({'architecture_function': get_dcgan_architectures, 'latent_prior_dist_class': DiagonalGaussianDistribution,
                                           'f_divergence_class': FDivKL, 'latent_dim': 100, 'mode': 'Alternate', 'batch_size': 32, 
                                           'group_freq_dict': {'Generator': 1, 'Critic': 1}})
-        optimization_args = {'Generator': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
+        optimization_args = {'Generator': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0002, 'learning_rate_decay_rate': 0.0001, 
                                                            'beta1': 0.5, 'beta2': 0.99, 'epsilon': 1e-07, 'gradient_clipping': 0}),
-                             'Critic': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
+                             'Critic': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0002, 'learning_rate_decay_rate': 0.0001, 
                                                         'beta1': 0.5, 'beta2': 0.99, 'epsilon': 1e-07, 'gradient_clipping': 0}),}
         
         return algorithm_args, optimization_args
@@ -45,14 +47,14 @@ def get_algorithm(algorithm_name):
         ## DCGAN
         from architectures import get_dcgan_architectures
         from distributions import DiagonalGaussianDistribution
-        from statistical_divergences import FDivReverseKL
+        from statistical_divergences import FDivReverseKL #reverse KL divergence as the f-divergence
 
         algorithm_args = helper.AttrDict({'architecture_function': get_dcgan_architectures, 'latent_prior_dist_class': DiagonalGaussianDistribution,
                                           'f_divergence_class': FDivReverseKL, 'latent_dim': 100, 'mode': 'Alternate', 'batch_size': 32, 
                                           'group_freq_dict': {'Generator': 1, 'Critic': 1}})
-        optimization_args = {'Generator': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
+        optimization_args = {'Generator': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0002, 'learning_rate_decay_rate': 0.0001, 
                                                            'beta1': 0.5, 'beta2': 0.99, 'epsilon': 1e-07, 'gradient_clipping': 0}),
-                             'Critic': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
+                             'Critic': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0002, 'learning_rate_decay_rate': 0.0001, 
                                                         'beta1': 0.5, 'beta2': 0.99, 'epsilon': 1e-07, 'gradient_clipping': 0}),}
         
         return algorithm_args, optimization_args
@@ -62,14 +64,14 @@ def get_algorithm(algorithm_name):
         ## DCGAN
         from architectures import get_dcgan_architectures
         from distributions import DiagonalGaussianDistribution
-        from statistical_divergences import FDivPearsonChiSquared
+        from statistical_divergences import FDivPearsonChiSquared #pearson chi-squared divergence as the f-divergence
 
         algorithm_args = helper.AttrDict({'architecture_function': get_dcgan_architectures, 'latent_prior_dist_class': DiagonalGaussianDistribution,
                                           'f_divergence_class': FDivPearsonChiSquared, 'latent_dim': 100, 'mode': 'Alternate', 'batch_size': 32, 
                                           'group_freq_dict': {'Generator': 1, 'Critic': 1}})
-        optimization_args = {'Generator': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
+        optimization_args = {'Generator': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0002, 'learning_rate_decay_rate': 0.0001, 
                                                            'beta1': 0.5, 'beta2': 0.99, 'epsilon': 1e-07, 'gradient_clipping': 0}),
-                             'Critic': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
+                             'Critic': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0002, 'learning_rate_decay_rate': 0.0001, 
                                                         'beta1': 0.5, 'beta2': 0.99, 'epsilon': 1e-07, 'gradient_clipping': 0}),}
         
         return algorithm_args, optimization_args
@@ -77,14 +79,14 @@ def get_algorithm(algorithm_name):
 
     elif algorithm_name == 'wGAN_gp-DCGAN':
         ## DCGAN
-        from architectures import get_dcgan_architectures_wgan
+        from architectures import get_dcgan_architectures_wgan #make sure to drop batchnorm layers in the critic
         from distributions import DiagonalGaussianDistribution
 
         algorithm_args = helper.AttrDict({'architecture_function': get_dcgan_architectures_wgan, 'latent_prior_dist_class': DiagonalGaussianDistribution, 
-                                          'latent_dim': 100, 'lambda_gp': 10, 'mode': 'Alternate', 'batch_size': 32, 'group_freq_dict': {'Generator': 1, 'Critic': 5}})
+                                          'latent_dim': 100, 'lambda_gp': 10, 'mode': 'Alternate', 'batch_size': 32, 'group_freq_dict': {'Generator': 1, 'Critic': 5}}) #as specified in the paper, we update the critic 5 times in each epoch while updating generator only once 
         optimization_args = {'Generator': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
-                                                           'beta1': 0, 'beta2': 0.9, 'epsilon': 1e-07, 'gradient_clipping': 0}),
+                                                           'beta1': 0, 'beta2': 0.9, 'epsilon': 1e-07, 'gradient_clipping': 0}), #Adam initialization params as specified in paper
                              'Critic': helper.AttrDict({'optimizer_class': 'Adam', 'learning_rate': 0.0001, 'learning_rate_decay_rate': 0.0001, 
-                                                        'beta1': 0, 'beta2': 0.9, 'epsilon': 1e-07, 'gradient_clipping': 0}),}
+                                                        'beta1': 0, 'beta2': 0.9, 'epsilon': 1e-07, 'gradient_clipping': 0}),} #Adam initialization params as specified in the paper
         
         return algorithm_args, optimization_args
